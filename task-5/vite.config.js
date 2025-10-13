df7720ea84cd491d754ad196389954a81f6f1ca3
Dave-WebDev <theyvidusoro@gmail.com>
2025-10-13 16:48:36 +0100

    deploy task 5 build

--- task-5/vite.config.js
+++ task-5/vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 // Adjust base so GitHub Pages serves assets correctly under /FlexiSaf-Internship/task-5/
 export default defineConfig({
-  base: '/FlexiSaf-Internship/task-5/'
-})
-
-
+  base: "/FlexiSaf-Internship/task-5/",
+});
